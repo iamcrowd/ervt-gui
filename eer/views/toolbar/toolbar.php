1affd86eb629ff4f3282de7181ecb3403cb3b042
German Braun <gab@gab.gab>
2019-12-02 12:03:35 -0300

    toolbar with dividers

--- eer/views/toolbar/toolbar.php
+++ eer/views/toolbar/toolbar.php
@@ -50,11 +50,11 @@
 	   </a>
 	    <div class="dropdown-menu" aria-labelledby="ervt-dropdown-btn">
           <a class="dropdown-item" href="#" id="export-json">Export to JSON</a>
-        <!--<div class="dropdown-divider"></div>-->
+        <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#" id="insert-query">Insert Query</a>
-        <!-- <div class="dropdown-divider"></div> -->
+        <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#" id="tdllitefpx">TDL-LiteFPX Encoding</a>
-        <!--<div class="dropdown-divider"></div>-->
+        <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#" id="sat">Satisfiability</a>
 	   </div>
   </div>
